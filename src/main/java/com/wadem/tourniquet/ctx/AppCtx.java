10bc0aba43f5a77cb78a6230d2553e227ac1c286
shaun.vallejo <shaun.vallejo@spartasystems.com>
2019-03-10 11:48:24 -0400

    Adding routing function

--- src/main/java/com/wadem/tourniquet/ctx/AppCtx.java
+++ src/main/java/com/wadem/tourniquet/ctx/AppCtx.java
@@ -1,8 +1,15 @@
 package com.wadem.tourniquet.ctx;
 
+import com.wadem.tourniquet.service.JITTrainingService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.*;
 
 @Configuration
 public class AppCtx {
-
+    @Bean
+    public RouterFunction<ServerResponse> route(JITTrainingService service) {
+        return RouterFunctions
+                .route(RequestPredicates.GET("/jittraining"),service::provideJIT);
+    }
 }
